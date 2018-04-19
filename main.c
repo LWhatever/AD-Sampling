f384d6996adacfd971d08336a2c2f053426b7a36
LWhatever <y15002783110@gmail.com>
2018-04-20 00:42:18 +0800

    Revert "Update main.c"

--- main.c
+++ main.c
@@ -1,24 +1,23 @@
 #include <msp430.h>
+#define   Num_of_Results   8
 
-char * Flash_ptr = (char *) 0x002400;
-char * Flash_ptr0 = (char *) 0x002400;
-volatile int write_mode = -1;
+volatile unsigned int A0results[Num_of_Results];
 
-void flash_write()
+int main(void)
 {
-	FCTL3 = FWKEY;                            // Clear Lock bit
-	FCTL1 = FWKEY+ERASE;                      // Set Erase bit
-	FCTL1 = FWKEY+WRT;
-	if(write_mode==1)
-		*Flash_ptr++ = ADC12MEM0;
-	if(Flash_ptr == (char *) 0x0043FF)
-		write_mode = 0;
-	FCTL1 = FWKEY;                            // Clear WRT bit
-	FCTL3 = FWKEY+LOCK;                       // Set LOCK bit
-}
+	volatile unsigned int i;
+	WDTCTL = WDTPW + WDTHOLD;
 
-void GPIO_Init()
-{
+	P6SEL |= BIT3;                            // P6.1 ADC option select
+	ADC12CTL0 = ADC12SHT0_4 + ADC12ON;         // Sampling time, ADC12 on
+	ADC12CTL1 = ADC12SHP + ADC12DIV_7;                     // Use sampling timer
+
+	ADC12CTL2 = ADC12RES_0;
+	ADC12MCTL0 = ADC12INCH_3;
+	ADC12IE = 0x01;                             // Enable interrupt
+	__delay_cycles(10000);
+	ADC12CTL0 |= ADC12ENC;
+
 	P4DIR |= 0xff;
 	P4OUT &= ~0xff;
 	P6DIR |= BIT6;
@@ -28,77 +27,43 @@ void GPIO_Init()
 	P5OUT &= ~(BIT0 + BIT1);
 	P2DIR |= BIT7;
 	P2OUT &= ~BIT7;
-	P8DIR |= BIT0;
-	P8OUT &= ~BIT0;
-
-	P3DIR |= BIT2;
-	P3OUT &= ~BIT2;
-
-
-	P2REN |= BIT6;
-	P2OUT |= BIT6;
-}
-
-int main(void)
-{
-	volatile unsigned int i;
-	WDTCTL = WDTPW + WDTHOLD;
-	P6SEL |= BIT3;                            // P6.1 ADC option select
-	ADC12CTL0 = ADC12ON + ADC12MSC;         // Sampling time, ADC12 on
-	ADC12CTL1 = ADC12CONSEQ1 + ADC12SHP;
-	ADC12CTL2 = ADC12RES_0;
-	ADC12MCTL0 = ADC12INCH_3;
-	ADC12CTL0 |= ADC12ENC;
-
-
-	TA0CTL |= MC_1 + TASSEL_2 + TACLR;
-	TA0CCTL0 = CCIE; //比较器中断使能
-	TA0CCR0 = 125; //比较值设为4096，相当于1s的时间间隔
-
-	TA1CCTL1 = CCIE;                          // CCR0 interrupt enabled
-	TA1CCR0 = 10000;
-	TA1CTL = TASSEL_1 + MC_1 + TACLR;         // SMCLK, upmode, clear TAR
-
-	GPIO_Init();
-	__bis_SR_register(GIE);
-
 	while(1)
 	{
-		ADC12CTL0 |= ADC12SC;      // Start sampling/conversion
-
-
+		ADC12CTL0 |= ADC12SC;                   // Start sampling/conversion
+		__bis_SR_register(GIE);       // Enter LPM0, Enable interrupts
 	}
 }
 //
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void TIMER0_A0_ISR(void)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
 {
-	P3OUT ^= BIT2;
-	if(write_mode == -1)
-		P4OUT = ADC12MEM0;
-	flash_write();
-}
-
-
-#pragma vector=TIMER1_A1_VECTOR
-__interrupt void TIMER1_A1_ISR(void)
-{
-	if(!(P2IN & BIT6))
+	switch(__even_in_range(ADC12IV,34))
 	{
-		__delay_cycles(100000);
-		P8OUT ^= BIT0;
-		write_mode = 1;
-		Flash_ptr0 = (char *) 0x002400;
-		Flash_ptr = (char *) 0x002400;
-	}
-	if(write_mode == 0)
-	{
-		P4OUT = *Flash_ptr0++;
-		if(Flash_ptr0 == Flash_ptr)
-		{
-			write_mode == -1;
-			Flash_ptr0 = (char *) 0x002400;
-			Flash_ptr = (char *) 0x002400;
-		}
+		case  0: break;                           // Vector  0:  No interrupt
+		case  2: break;                           // Vector  2:  ADC overflow
+		case  4: break;                           // Vector  4:  ADC timing overflow
+		case  6:                                  // Vector  6:  ADC12IFG0
+			if(ADC12MEM0>128)
+				P2OUT |= BIT7;
+			else
+				P2OUT &= ~BIT7;
+
+			P4OUT = ADC12MEM0;
+			 __no_operation();
+		case  8: break;                           // Vector  8:  ADC12IFG1
+		case 10: break;                           // Vector 10:  ADC12IFG2
+		case 12: break;                           // Vector 12:  ADC12IFG3
+		case 14: break;                           // Vector 14:  ADC12IFG4
+		case 16: break;                           // Vector 16:  ADC12IFG5
+		case 18: break;                           // Vector 18:  ADC12IFG6
+		case 20: break;                           // Vector 20:  ADC12IFG7
+		case 22: break;                           // Vector 22:  ADC12IFG8
+		case 24: break;                           // Vector 24:  ADC12IFG9
+		case 26: break;                           // Vector 26:  ADC12IFG10
+		case 28: break;                           // Vector 28:  ADC12IFG11
+		case 30: break;                           // Vector 30:  ADC12IFG12
+		case 32: break;                           // Vector 32:  ADC12IFG13
+		case 34: break;                           // Vector 34:  ADC12IFG14
+		default: break;
 	}
 }
